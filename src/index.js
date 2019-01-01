3e65bd6546256571cbbbb1bb4402a638197ba54c
Harry Gothold <harrygot@hotmail.com>
2019-01-01 14:18:08 +0000

    started adding layout components

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-// let's go!
-alert('hey');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './css/style.css';
+
+
+
+ReactDOM.render(<App />, document.getElementById('main'));
